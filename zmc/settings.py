a001f63f8dd2717b5106e0c3447269b314d95853
ian cuevas <iancuevas7321@gmail.com>
2026-02-21 17:02:19 -0300

    Vista local en telefono

--- zmc/settings.py
+++ zmc/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = "django-insecure-$g#v#&i6&-92#$f*bdms70h40ym(94x%7x^)bsw5q2atvvnk5=
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['192.168.1.15', 'localhost', '127.0.0.1', '*']
 
 
 # Application definition
